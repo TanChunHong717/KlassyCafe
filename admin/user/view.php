0fc3c9ad8427f230199b44ddfc37f1d12f892186
TanChunHong <tanchunhong717@gmail.com>
2023-06-01 13:34:38 +0800

    Update admin/user/view.php

--- admin/user/view.php
+++ admin/user/view.php
@@ -42,8 +42,8 @@
                             <li><a href="../index.php">Admin Home</a></li>
                             <li><a href="../menu/view.php">Menu</a></li>
                             <li><a href="../table/view.php">Table</a></li>
-                            <li><a href="../../logout.php">logout</a></li>
                             <li><a>User</a></li>
+                            <li><a href="../../logout.php">Logout</a></li>
                         </ul>        
                         <!-- ***** Menu End ***** -->
                     </nav>
